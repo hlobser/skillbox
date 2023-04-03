7affcc79334df1e3ca84466b4f59c1c8376932da
Сергей Хлобостин <hlobser@gmail.com>
2023-04-03 09:26:55 +0300

    решил полностью

--- lesson 2/06_songs_list.py
+++ lesson 2/06_songs_list.py
@@ -21,6 +21,11 @@ violator_songs_list = [
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
 # TODO здесь ваш код
+song_halo=violator_songs_list[3]
+song_enjoy_the_silence=violator_songs_list[5]
+song_clean=violator_songs_list[8]
+sound_duration=song_halo[1]+song_clean[1]+song_enjoy_the_silence[1]
+print('Три песни звучат ',sound_duration,'минут')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -39,3 +44,8 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+sweetest_perfection=violator_songs_dict ['Sweetest Perfection']
+policy_of_truth= violator_songs_dict ['Policy of Truth']
+blur_dress= violator_songs_dict['Blue Dress']
+another_sound_duration=sweetest_perfection+policy_of_truth+blur_dress
+print('А другие три песни звучат ',round(another_sound_duration,2),'минут')
