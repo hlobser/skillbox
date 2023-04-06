254c6961ce5e8cb3b2b64d9f0951c7815edd39f5
Сергей Хлобостин <hlobser@gmail.com>
2023-04-06 09:38:53 +0300

    добавил параметры функции, сделал зависимости координат глаз и рта от вводимых параметров функции

--- lesson 3/08_smile.py
+++ lesson 3/08_smile.py
@@ -3,6 +3,7 @@
 # (определение функций)
 import simple_draw
 
+
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
@@ -12,23 +13,24 @@ import simple_draw
 def point(x, y):
     simple_draw.get_point(x, y)
     return simple_draw.get_point(x, y)
-def smile_random():
+
+
+def smile_random(x, y, color):
     x1 = simple_draw.random_number(0, 400)
     y1 = simple_draw.random_number(0, 400)
-    purple = simple_draw.COLOR_PURPLE
     ellipse_start = point(x1, y1)
-    ellipse_finish = point(x1+150, y1+100)
-    simple_draw.ellipse(ellipse_start, ellipse_finish, purple, width=1)
-    left_eye_center = point(x1+45, y1+60)
-    right_eye_center = point(x1+105, y1+60)
-    simple_draw.circle(left_eye_center, radius=5, color=purple, width=1)
-    simple_draw.circle(right_eye_center, radius=5, color=purple, width=1)
-    mouth_coordinates_list = [point(x1 + 40, y1 + 30), point(x1 + 50, y1 + 20), point(x1 + 100, y1 + 20), point(x1 + 110, y1 + 30)]
-    simple_draw.lines(point_list=mouth_coordinates_list, color=purple)
+    ellipse_finish = point(x1 + x, y1 + y)
+    simple_draw.ellipse(ellipse_start, ellipse_finish, color, width=1)
+    left_eye_center = point(x1 + (x / 2) + x / 4, y1 + y / 2 + y / 8)
+    right_eye_center = point(x1 + (x / 2) - x / 4, y1 + y / 2 + y / 8)
+    simple_draw.circle(left_eye_center, radius=5, color=color, width=1)
+    simple_draw.circle(right_eye_center, radius=5, color=color, width=1)
+    mouth_coordinates_list = [point(x1 + (x / 2) + x / 4, y1 + y / 4), point(x1 + (x / 2) + x / 5, y1 + y / 5),
+                              point(x1 + (x / 2) - x / 5, y1 + y / 5), point(x1 + (x / 2) - x / 4, y1 + y / 4)]
+    simple_draw.lines(point_list=mouth_coordinates_list, color=color)
 
 
 for _ in range(10):
-    print(smile_random())
-
+    print(smile_random(100, 300, color=simple_draw.COLOR_RED))
 
 simple_draw.pause()
