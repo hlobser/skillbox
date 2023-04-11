69ff91fafba8bcc834bdf34a2470fd337a642ba8
Сергей Хлобостин <hlobser@gmail.com>
2023-04-11 09:26:40 +0300

    complete

--- lesson 4/03_shape_select.py
+++ lesson 4/03_shape_select.py
@@ -11,4 +11,32 @@ import simple_draw as sd
 
 # TODO здесь ваш код
 
+color = sd.COLOR_PURPLE
+def point(x, y):
+    sd.get_point(x, y)
+    return sd.get_point(x, y)
+
+
+def polygon(number_of_angles, start_point, angle, length, color):
+    for i in range(1, number_of_angles+1):
+        vector = sd.vector(start=start_point, angle=angle + (360 / number_of_angles * i),
+                           length=length, color=color)
+        start_point = vector
+    return vector
+
+
+print('Возможные фигуры:\n'
+      '0: треугольник\n'
+      '1: квадрвт\n'
+      '2: пятиугольник\n'
+      '3: шестиугольник')
+print('введите желаемую фигуру:')
+num_of_polygon = int(input())
+for j in range(0, 4):
+    if j == num_of_polygon:
+        polygon(number_of_angles=j+3, start_point=point(350, 250), angle=0, length=100, color=color)
+        break
+else:
+    print('вы ввели некорректный номер')
+
 sd.pause()
