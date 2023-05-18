0b344cd419630baf0d74b8642f1f37cd5cbf13c8
Сергей Хлобостин <hlobser@gmail.com>
2023-05-18 13:31:49 +0300

    Сделал

--- lesson 7/02_alchemy.py
+++ lesson 7/02_alchemy.py
@@ -19,8 +19,100 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
+class Water:
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        if isinstance(other, Fire):
+            return Steam()
+        if isinstance(other, Ground):
+            return Dirt()
+        if isinstance(other, Water):
+            return Water()
+    def __str__(self):
+        return 'Вода'
+
+
+class Fire:
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Lightning()
+        if isinstance(other, Water):
+            return Steam()
+        if isinstance(other, Ground):
+            return Lava()
+        if isinstance(other, Fire):
+            return Fire()
+    def __str__(self):
+        return 'Огонь'
+
+
+class Ground:
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Dust()
+        if isinstance(other, Fire):
+            return Lava()
+        if isinstance(other, Water):
+            return Dirt()
+        if isinstance(other, Ground):
+            return Ground()
+    def __str__(self):
+        return 'Земля'
+
+
+class Air:
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        if isinstance(other, Fire):
+            return Lightning()
+        if isinstance(other, Ground):
+            return Dust()
+        if isinstance(other, Air):
+            return Air()
+    def __str__(self):
+        return 'Воздух'
+
+
+class Steam:
+    def __str__(self):
+        return 'Пар'
+
+
+class Storm:
+    def __str__(self):
+        return 'Шторм'
+
+
+class Dirt:
+    def __str__(self):
+        return 'Грязь'
+
+
+class Lightning:
+    def __str__(self):
+        return 'Молния'
+
+
+class Dust:
+    def __str__(self):
+        return 'Пыль'
+
+
+class Lava:
+    def __str__(self):
+        return 'Лава'
+
+
+print(Water(), '+', Fire(), '=', Water() + Fire())
+print(Ground(), '+', Water(), '=', Ground() + Water())
+print(Water(), '+', Water(), '=', Water() + Water())
+print(Water(), '+', Lava(), '=', Water() + Lava())
 # TODO здесь ваш код
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
 # Придумать что будет при сложении существующих элементов с новым.
+
+
