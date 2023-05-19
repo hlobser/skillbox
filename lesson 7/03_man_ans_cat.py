2dd52e4f074199b26b7caecae327bd9f100b32c0
Сергей Хлобостин <hlobser@gmail.com>
2023-05-19 15:04:03 +0300

    Муся заехала в дом к Кенни

--- lesson 7/03_man_ans_cat.py
+++ lesson 7/03_man_ans_cat.py
@@ -4,6 +4,192 @@ from random import randint
 
 # Доработать практическую часть урока lesson_007/python_snippets/08_practice.py
 
+from random import randint
+
+
+# Реализуем модель человека.
+# Человек может есть, работать, играть, ходить в магазин.
+# У человека есть степень сытости, немного еды и денег.
+# Если сытость < 0 единиц, человек умирает.
+# Человеку надо прожить 365 дней.
+from termcolor import cprint
+
+
+class Man:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+        self.cat = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='blue')
+        self.house.money += 150
+        self.fullness -= 10
+
+    def watch_MTV(self):
+        cprint('{} смотрел MTV целый день'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+            self.house.money -= 50
+            self.house.food += 50
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        cprint('{} Вьехал в дом'.format(self.name), color='cyan')
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.house.food < 10:
+            self.shopping()
+        elif self.house.dirt > 100:
+            self.clean_house()
+        # elif self.house.cat_food < 10:
+        #     self.buy_cat_food()
+        elif self.house.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.watch_MTV()
+
+    def pick_up_a_cat(self, name):
+        self.cat = name
+        self.cat.owner = self
+        self.cat.house = self.house
+        cprint('{} Подобрал кота и назвал его {}' .format(self.name, self.cat.name), color='cyan')
+
+    def buy_cat_food(self):
+        self.house.cat_food += 50
+        self.house.money -= 50
+        cprint('{} сходил в магазин за едой для кошки'.format(self.name), color='magenta')
+
+    def clean_house(self):
+        self.house.dirt -= 100
+        self.fullness -= 20
+        cprint('{} убрался в доме'.format(self.name), color='cyan')
+
+
+class House:
+
+    def __init__(self):
+        self.food = 50
+        self.money = 0
+        self.cat_food = 0
+        self.dirt = 0
+
+    def __str__(self):
+        return 'В доме еды осталось {},  денег осталось {}, кошачей еды {}, грязи {}'.format(
+            self.food, self.money, self.cat_food, self.dirt)
+
+
+class Cat:
+    def __init__(self, name):
+        self.house = None
+        self.name = name
+        self.owner = None
+        self.fullness = 50
+
+    def __str__(self):
+        return '*мяу Я -  кошка {}, сытость {} мяу*'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.cat_food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 20
+            self.house.cat_food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+
+    def sleep(self):
+        cprint('{} спал целый день'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def rip_wallpaper(self):
+        cprint('{} подрал обои'.format(self.name), color='green')
+        self.fullness -= 10
+        self.house.dirt += 5
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.house.cat_food < 10:
+            self.owner.buy_cat_food()
+        elif dice == 1:
+            self.rip_wallpaper()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.sleep()
+
+
+citizens = [
+    Man(name='Бивис'),
+    Man(name='Батхед'),
+    Man(name='Кенни'),
+]
+
+
+my_sweet_home = House()
+for citisen in citizens:
+    citisen.go_to_the_house(house=my_sweet_home)
+
+musya = Cat(name='Муся')
+citizens[2].pick_up_a_cat(musya)
+
+
+
+for day in range(1, 366):
+    print('================ день {} =================='.format(day))
+    for citisen in citizens:
+        citisen.act()
+    musya.act()
+    print('--- в конце дня ---')
+    for citisen in citizens:
+        print(citisen)
+    print(musya)
+
+
+    print(my_sweet_home)
+
+
+# Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
+# Нужен класс Дом, в нем должн быть холодильник с едой и тумбочка с деньгами
+# Еда пусть хранится в холодильнике в доме, а деньги - в тумбочке.
+
+
+
 # Необходимо создать класс кота. У кота есть аттрибуты - сытость и дом (в котором он живет).
 # Кот живет с человеком в доме.
 # Для кота дом характеризируется - миской для еды и грязью.
@@ -24,6 +210,7 @@ from random import randint
 # что будет делать сегодня
 
 # Человеку и коту надо вместе прожить 365 дней.
+
 
 # TODO здесь ваш код
 
--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-list_input = input().split()
-for k in range(len(list_input)):
-    list_input[k] = int(list_input[k])
-count = 0
-for i in range(len(list_input) - 1):
-    for j in range(len(list_input) - i - 1):
-        if list_input[j + 1] > list_input[j]:
-          count += 1
-    break
-print(count)
+row = input().split()
+result = [[]]
+for i in range(1, len(row) + 1):
+    for j in range(len(row) - i + 1):
+        result.append(row[j: i + j])
+
+print(result)
