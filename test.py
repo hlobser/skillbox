2a1247c04b188c5b64dbbe3b06a7d0478c77d021
Сергей Хлобостин <hlobser@gmail.com>
2023-05-24 08:36:44 +0300

    произведение матриц

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-row = input().split()
-result = [[]]
-for i in range(1, len(row) + 1):
-    for j in range(len(row) - i + 1):
-        result.append(row[j: i + j])
+n1, m1 = [int(i) for i in input().split()]
+matrix_1 = [[int(i) for i in input().split()] for _ in range(n1)]
+spase = input()
+n2, m2 = [int(i) for i in input().split()]
+matrix_2 = [[int(i) for i in input().split()] for _ in range(n2)]
 
-print(result)
+matrix_product = [[0] * m2 for _ in range(n1)]
+for i_product in range(n1):
+    for j_product in range(m2):
+        for j in range(m1):
+            matrix_product[i_product][j_product] += matrix_1[i_product][j]*matrix_2[j][j_product]
+
+
+for k in matrix_product:
+    print(*k)
