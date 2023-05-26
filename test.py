f85ace37f09d87cc0de48829bc277664d3bb17a1
Сергей Хлобостин <hlobser@gmail.com>
2023-05-26 15:52:58 +0300

    новая ветка develop fuck off

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-from copy import deepcopy
-n = int(input())
-matrix1 = [[int(i) for i in input().split()] for _ in range(n)]
-m = int(input())
-matrix2 = deepcopy(matrix1)
-for _ in range(1, m):
-    result_matrix = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                result_matrix[i][j] += matrix1[i][k]*matrix2[k][j]
-    matrix2 = deepcopy(result_matrix)
-
-for row in result_matrix:
-    print(*row)
+m, n = int(input()), int(input())
+first_set = set([input() for _ in range(n)])
+empty_set = set()
+if m == 1:
+    print(*sorted(first_set), sep='\n')
+else:
+    for i in range(m-1):
+        for j in range(int(input())):
+            empty_set.add(input())
+        first_set.intersection_update(empty_set)
+        empty_set.clear()
+    first_set = sorted(first_set)
+    for row in first_set:
+        print(row)
+fuck off
