d89c675afc27077bd477e12c79a15a163ff40fd6
Сергей Хлобостин <hlobser@gmail.com>
2023-05-31 15:44:34 +0300

    Сделал, упорядочивание - по частоте по убыванию

--- lesson 9/01_char_stat.py
+++ lesson 9/01_char_stat.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import zipfile
+
 
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
@@ -17,11 +19,58 @@
 # +---------+----------+
 # |  итого  | 9999999  |
 # +---------+----------+
-#
+
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
 # TODO здесь ваш код
+
+class Statistic:
+
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+        self.count = 0
+
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+
+    def collect(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        self.stat[char] = self.stat.get(char, 0) + 1
+                        self.count += 1
+
+    def sorting(self):
+        self.stat = dict(sorted(self.stat.items(), key=lambda x: (x[1], x[0]), reverse=True))
+
+    def output_table(self):
+        print(f'''+---------+----------+
+|  буква  | частота  |
++---------+----------+''')
+        for key, value in self.stat.items():
+            print(f'''|    {key}    |   {value:6d}  |''')
+        print(f'''+---------+----------+
+|  итого  | {self.count}  |
++---------+----------+''')
+
+
+stasistic = Statistic(file_name='python_snippets\\voyna-i-mir.txt.zip')
+stasistic.unzip()
+stasistic.collect()
+stasistic.sorting()
+stasistic.output_table()
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
