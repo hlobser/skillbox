dba6d8441a7f7ff945170baab92ad5e140adf908
Сергей Хлобостин <hlobser@gmail.com>
2023-06-01 14:41:49 +0300

    сделал без классов

--- lesson 9/02_log_parser.py
+++ lesson 9/02_log_parser.py
@@ -20,6 +20,32 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
 # TODO здесь ваш код
+with open('events.txt', mode='r', encoding='utf8') as log_file:
+    minute_prev_line = None
+    prev_date = None
+    count = 0
+    for line in log_file:
+        date, event = line[:line.index(' ') + 6], line[line.index(']')+1:]
+        minute_this_line = date[-1]
+        if minute_prev_line is None and prev_date is None:
+            minute_prev_line = minute_this_line
+            prev_date = date
+            if 'NOK' in event:
+                count += 1
+        else:
+            if minute_this_line == minute_prev_line:
+                if 'NOK' in event:
+                    count += 1
+            else:
+                if count:
+                    with open('count_of_events.txt', mode='a', encoding='utf8') as count_file:
+                        text = f'{date}] {str(count)}\n'
+                        count_file.write(text)
+                minute_prev_line = minute_this_line
+                prev_date = date
+                count = 0
+                if 'NOK' in event:
+                    count += 1
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
