a99f6a471b0d3098993fff9ee19bd52163576279
Сергей Хлобостин <hlobser@gmail.com>
2023-06-07 10:51:54 +0300

    сделал, через логгер не получилось, не выводит класс ошибки

--- lesson 10/02_groundhog_day.py
+++ lesson 10/02_groundhog_day.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import logging
+from random import randint
+
 
 # День сурка
 #
@@ -21,4 +24,59 @@ ENLIGHTENMENT_CARMA_LEVEL = 777
 
 # TODO здесь ваш код
 
+
+class IamGodError(Exception):
+    pass
+
+
+class DrunkError(Exception):
+    pass
+
+
+class CarCrashError(Exception):
+    pass
+
+
+class GluttonyError(Exception):
+    pass
+
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
+
+def one_day():
+    karma_point = randint(1, 7)
+    try:
+        if karma_point == 1:
+            raise IamGodError('Если бог существует, то это, мать вашу, Я!!!АХАХАХАХ')
+        if karma_point == 2:
+            raise DrunkError('Лучшая зарядка это балтика девятка')
+        if karma_point == 3:
+            raise CarCrashError('Привыкни к аппарату. Не топи, блять!!!')
+        if karma_point == 4:
+            raise GluttonyError('Две мясных котлеты гриль, специальный соус, сыр...')
+        if karma_point == 5:
+            raise DepressionError('Сначала парни говорят что депрессии нет, а потом сидят целый день и смотрят в одну точку')
+        if karma_point == 6:
+            raise SuicideError('Ребята, смотрите, я черешня')
+    except:
+        random_raise = randint(1, 13)
+        if random_raise == 1:
+            raise
+    return karma_point
+
+# logging.basicConfig(filename="log.txt", level = logging.ERROR, encoding='utf8')
+karma = 0
+while karma < ENLIGHTENMENT_CARMA_LEVEL:
+    try:
+        karma += one_day()
+    except (IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError) as exc:
+        with open(file='log.txt', mode='a', encoding='utf8') as file:
+            file.write(f'{type(exc)}: {exc}' + '\n')
+        # logging.error(exc)
 # https://goo.gl/JnsDqu
