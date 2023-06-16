e88a5431aafa7cc8ae3630458be0aec12d9060fc
Сергей Хлобостин <hlobser@gmail.com>
2023-06-16 10:47:05 +0300

    сделал через генератор, через итератор не получилось

--- lesson 11/03_log_parser.py
+++ lesson 11/03_log_parser.py
@@ -15,3 +15,33 @@
 # [2018-05-17 01:57] 1234
 
 # TODO здесь ваш код
+def log_parser():
+    with open('events.txt', mode='r', encoding='utf8') as log_file:
+        minute_prev_line = None
+        prev_date = None
+        count = 0
+        for line in log_file:
+            date, event = line[:line.index(' ') + 6], line[line.index(']')+1:]
+            minute_this_line = date[-1]
+            if minute_prev_line is None and prev_date is None:
+                minute_prev_line = minute_this_line
+                prev_date = date
+                if 'NOK' in event:
+                    count += 1
+            else:
+                if minute_this_line == minute_prev_line:
+                    if 'NOK' in event:
+                        count += 1
+                else:
+                    if count:
+                        yield prev_date + ']', str(count)
+                    minute_prev_line = minute_this_line
+                    prev_date = date
+                    count = 0
+                    if 'NOK' in event:
+                        count += 1
+
+
+grouped_events = log_parser()
+for group_time, event_count in grouped_events:
+    print(f'{group_time} {event_count}')
