d002e2facc202c11afb95c1b9634064d703a1a05
Сергей Хлобостин <hlobser@gmail.com>
2023-06-26 11:02:43 +0300

    сделал

--- lesson 13/01_ticket.py
+++ lesson 13/01_ticket.py
@@ -10,9 +10,20 @@ from PIL import Image, ImageDraw, ImageFont, ImageColor
 # Подходящий шрифт искать на сайте ofont.ru
 
 def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+    im = Image.open('images/ticket_template.png')
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype('ofont.ru_Kanit Cyrillic.ttf', size=15)
+    y = im.size[1]
+    draw.text((45, y - 275), fio, font=font, fill=ImageColor.colormap['black'])
+    draw.text((45, y - 205), from_, font=font, fill=ImageColor.colormap['black'])
+    draw.text((45, y - 140), to, font=font, fill=ImageColor.colormap['black'])
+    draw.text((285, y - 140), date, font=font, fill=ImageColor.colormap['black'])
+    # im.show()
+    im.save('ticket.png')
 
+
+
+make_ticket(fio='Пригожин Е.В.', from_='Ростов-на-Дону', to='Минск', date='24.06')
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля agrparse.
 # Скрипт должен принимать параметры:
