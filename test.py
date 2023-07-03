ebc4927f5a8eb0b42d249dc4a7ded3c481fbf63a
Sergey Khlobostin <hlobser@gmail.com>
2023-07-03 10:43:14 +0300

    filr

--- test.py
+++ test.py
@@ -1,15 +1,6 @@
-m, n = int(input()), int(input())
-first_set = set([input() for _ in range(n)])
-empty_set = set()
-if m == 1:
-    print(*sorted(first_set), sep='\n')
-else:
-    for i in range(m-1):
-        for j in range(int(input())):
-            empty_set.add(input())
-        first_set.intersection_update(empty_set)
-        empty_set.clear()
-    first_set = sorted(first_set)
-    for row in first_set:
-        print(row)
-fuck off
+import re
+with open(r'C:\Users\Sergey\projects\skillbox\nums.txt', encoding='utf-8', mode='r') as file:
+    sp = [re.findall('\d+', i) for i in file.readlines()]
+    print(sum([int(i)  for lst in sp for i in lst]))
+123
+sdfgsd
