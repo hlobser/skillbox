b8a90429d06dfca9bdc6e00fcba22998936c90ee
Sergey Khlobostin <hlobser@gmail.com>
2023-07-03 10:44:50 +0300

    lalala

--- test.py
+++ test.py
@@ -2,5 +2,4 @@ import re
 with open(r'C:\Users\Sergey\projects\skillbox\nums.txt', encoding='utf-8', mode='r') as file:
     sp = [re.findall('\d+', i) for i in file.readlines()]
     print(sum([int(i)  for lst in sp for i in lst]))
-123
-sdfgsd
+lalalalalala
